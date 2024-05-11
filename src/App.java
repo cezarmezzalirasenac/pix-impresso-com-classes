4cddc1b1990cc8c0b3d1da704bab2e7d0932fc51
Cezar Augusto Mezzalira <cezar.mezzalira@docente.pr.senac.br>
2024-05-10 21:49:08 -0300

    Entrada de dados

--- src/App.java
+++ src/App.java
@@ -1,5 +1,59 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import classes.Conta;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        ArrayList<Conta> contas = new ArrayList<Conta>();
+
+        contas.add(new Conta(1, 123,
+                1234, 0,
+                "Cezar Mezzalira",
+                "123.456.789-00", 1,
+                "1234567891234567", "123"));
+
+        Scanner scanner = new Scanner(System.in);
+        // ENTRADA
+        // 1 - Ir até um caixa eletrônico
+        // 2 - Inserir o cartão
+        // 2.1 - Ler os dados do cartão
+        System.out.println("Insira o numero do cartão: ");
+        String numeroCartao = scanner.nextLine();
+
+        Conta contaSaque = null;
+        for (Conta conta : contas) {
+            // se o número do cartão for igual ao número do cartão de uma conta
+            if (conta.getNumeroCartao().equals(numeroCartao)) {
+                contaSaque = conta;
+                break;
+            }
+        }
+
+        if (contaSaque == null) {
+            throw new Error("Conta não encontrada");
+        }
+
+        // 3 - Inserir a senha
+        System.out.println("Insira a senha: ");
+        String senha = scanner.nextLine();
+
+        // 4 - Validar a senha (processamento intermediario)
+        // se a senha estiver incorreta, vai mostrar um erro e terminar o programa
+        if (!contaSaque.getSenha().equals(senha)) {
+            throw new Error("Senha inválida");
+        }
+        // 5 - Informar o valor do saque
+
+        // PROCESSAMENTO
+        // 6 - Validar se o caixa possui saldo
+        // 7 - Validar se a conta possui saldo
+        // 8 - Liberar o dinheiro para o cliente
+        // 9 - Registrar a transação
+
+        // SAÍDA
+        // 10 - Encerrar a operação
+        // 10.1 - Mostrar o comprovante da transação em tela
+        scanner.close();
     }
 }
--- src/classes/Conta.java
+++ src/classes/Conta.java
@@ -11,4 +11,58 @@ public class Conta {
   private int tipoConta;
   private String numeroCartao;
   private String senha;
+
+  public Conta(int idConta, int numero,
+      int agencia, int digitoVerificador,
+      String nome, String cpf, int tipoConta,
+      String numeroCartao, String senha) {
+    this.idConta = idConta;
+    this.numero = numero;
+    this.agencia = agencia;
+    this.digitoVerificador = digitoVerificador;
+    this.nome = nome;
+    this.cpf = cpf;
+    this.tipoConta = tipoConta;
+    this.numeroCartao = numeroCartao;
+    this.senha = senha;
+  }
+
+  public int getIdConta() {
+    return idConta;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public int getAgencia() {
+    return agencia;
+  }
+
+  public int getDigitoVerificador() {
+    return digitoVerificador;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public int getTipoConta() {
+    return tipoConta;
+  }
+
+  public String getNumeroCartao() {
+    return numeroCartao;
+  }
+
+  public String getSenha() {
+    return senha;
+  }
+
+  
+
 }
